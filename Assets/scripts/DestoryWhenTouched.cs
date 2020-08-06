cda3251c3920c4c125429aaf60d2f81dd5ee7e97
Juggernaut9434 <juggernaut9434@gmail.com>
2020-08-06 16:02:36 -0700

    Coin Destory Script When you want a trigger, make sure to check the checkbox under Collider for isTrigger, otherwise it won't work Saved Destory Coin to the prefab for easy access

---
+++ Assets/scripts/DestoryWhenTouched.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestoryWhenTouched : MonoBehaviour
+{
+    /*
+    Destory's game object when collided.
+    Make sure to turn on isTrigger under Collider in Unity
+    */
+
+    // happens on trigger event is true
+    void OnTriggerEnter(Collider other)
+    {
+        // destroys the object thats not attached to the script
+        // Destroy(other.gameObject)
+
+        // Destroys the object that IS attached to script
+        Destroy(gameObject);
+    }
+
+    /* Happens on trigger event after true but now false
+    void OnTriggerExit(Collider other) {}
+    */
+    void Start() {}
+    void Update() {}
+}
