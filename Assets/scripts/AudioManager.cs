5d5360d700efad837f03df545ed1b0ec346ee41f
Juggernaut9434 <juggernaut9434@gmail.com>
2020-08-09 11:28:55 -0700

    AudioManager and Renaming Folders AudioManager and Sound scripts Renamed Prefabs1 to MyPrefabs

--- Assets/scripts/AudioManager.cs
+++ Assets/scripts/AudioManager.cs
@@ -1,18 +1,60 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.Audio;
 using UnityEngine;
+using System;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Sound[] sounds;
+
+    // in the case of multiple audioManager gameObjects
+    public static AudioManager instance;
+
+    // Awake is like Start() but is called right before
+    void Awake()
     {
-        
+        // in the case of multiple audioManager gameObjects
+        if(instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // making the AudioManager persist through Loading Scenes
+        DontDestroyOnLoad(gameObject);
+
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        // Any sounds you want to play from the start of the game
+        // Play("MainSoundTrack");
+    }
+
+    public void Play(string name)
+    {
+        // using System
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        // In the case of typo's and other null references
+        if (s == null) {
+            Debug.LogWarning("Sound: " + name + " was not found!");
+            return;
+        }
+        s.source.Play();
+
+        /*
+        In the script for an object to make a sound
+        you can use:
+        FindObjectOfType<AudioManager>().Play("Sound");     // if the audio is called 'Sound'
+        */
     }
 }
---
+++ Assets/scripts/Sound.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]   // custom class allows to be found by unity
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+
+    [Range(0f, 1f)]     // adding slides to unity
+    public float volume;
+    [Range(0.1f, 3f)]
+    public float pitch;
+
+    public bool loop;
+
+    [HideInInspector]   // Hide b/c we automatically create in in AudioManager
+    public AudioSource source;  // allows to use the Play() method
+
+}
